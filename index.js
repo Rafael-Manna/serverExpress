0c7e86c2c508921232a8af612bf559e3f6ab7eef
Rafael-Manna <rafaelmannager@gmail.com>
2026-05-05 21:21:59 -0300

    :sparkles:

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import path from "path";
 const server = express();
 const PORT = 3000;
 const HOST = "localhost";
+.
+let cursos = [{ curso: "Desenvolvimento Web", ch: 80, tipo: "EAD" }]; // Array para armazenar os cursos cadastrados
 
-
-app.use(express.json());// middleware
-app.use(express.urlencoded({ extended: true })); // middleware para processar dados de formulários
-
+server.use(express.json());// middleware
+server.use(express.urlencoded({ extended: true })); // middleware para processar dados de formulários
 
 server.get("/", (req, res) => {
   res.send(`
@@ -20,6 +20,14 @@ server.get("/", (req, res) => {
 
 server.get("/cadastro", (req, res) => {
     res.sendFile(path.resolve("cadastro.html"));
+});
+
+server.post("/curso", (req, res) => {
+    const curso = req.body.curso;
+    const ch = req.body.ch;
+    const tipo = req.body.tipo;
+    cursos.push({ curso, ch, tipo }); // Adiciona o novo curso ao array de cursos
+    res.status(200).json({ mensagem: "Curso cadastrado com sucesso!", curso, ch, tipo }); // Envia uma resposta de sucesso
 });
 
 server.listen(PORT, HOST, () => {    
